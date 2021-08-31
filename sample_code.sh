4b53c8873a0ad0cf30c8ed5f7ef56cb5d9a0b7a8
yashwanthpanjala <89287253+yashwanthpanjala@users.noreply.github.com>
2021-08-31 09:16:38 +0530

    firstcommit

--- sample_code.sh
+++ sample_code.sh
@@ -3,7 +3,7 @@
 
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
-echo "This is an example of a script"
+echo "This is an example of a script languge"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
